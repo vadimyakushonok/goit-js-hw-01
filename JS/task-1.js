207c6bae0860a9619b2cbee2de330e897564822a
Vadim <vyakushonok@gmail.com>
2020-12-22 19:31:36 +0300

    Коммит первого задания

---
+++ JS/task-1.js
@@ -0,0 +1,10 @@
+const name = '«Генератор защитного поля»';
+let price = 1000;
+const firstInformation = `Выбран ${name}, цена за штуку ${price} кредитов`
+
+console.log (firstInformation);
+
+price = 2000;
+const secondInformation = `Выбран ${name}, цена за штуку ${price} кредитов`;
+
+console.log (secondInformation);
