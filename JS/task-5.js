8b1d696622800731d966ed8dbb3ab77e7a591cc2
Vadim <vyakushonok@gmail.com>
2020-12-29 15:23:24 +0300

    коммит на 5 задании

--- JS/task-5.js
+++ JS/task-5.js
@@ -1,8 +1,15 @@
 const contryName = prompt ('Введите название страны для расчета стоимости доставки');
 
+const China = 'Китай';
+const Chili = 'Чили';
+const Australia = 'Австалия';
+const India = 'Индия';
+const Jamaica = 'Ямайка';
+
+
+
 const China = 100;
 const Chili = 250;
 const Australia = 170;
 const India = 80;
 const Jamaica = 120;
-
