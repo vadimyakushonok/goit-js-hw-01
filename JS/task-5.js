687fcaad5bd43ec283ff3e0acba0c2878aa2a0d5
Vadim <vyakushonok@gmail.com>
2020-12-30 13:36:54 +0300

    Последний коммит

--- JS/task-5.js
+++ JS/task-5.js
@@ -2,14 +2,50 @@ const contryName = prompt ('Введите название страны для 
 
 const China = 'Китай';
 const Chili = 'Чили';
-const Australia = 'Австалия';
+const Australia = 'Австралия';
 const India = 'Индия';
 const Jamaica = 'Ямайка';
 
+let message;
+let price;
+let country;
 
+const cancelByUser = 'Отменено пользователем!';
+const noDelivery = 'В вашей стране доставка не доступна';
+/* const orderCompleted = `Доставка в ${contry} будет стоить ${price} кредитов`; */
 
-const China = 100;
-const Chili = 250;
-const Australia = 170;
-const India = 80;
-const Jamaica = 120;
+if (contryName === null) {
+    alert(cancelByUser);
+} else { 
+    country = contryName[0].toUpperCase() + contryName.slice(1).toLowerCase(); 
+    switch (country) {
+            case China:
+                price = 100;
+                break;
+    
+            case Chili:
+                price = 250;
+                break;
+    
+            case Australia:
+                price = 170;
+                break;
+    
+            case India:
+                price = 80;
+                break;
+    
+            case Jamaica:
+                price = 120;
+                break;
+                
+            default:
+                alert (noDelivery);
+    }
+  }
+
+  if (price > 0) {
+    message = `Доставка в ${country} будет стоить ${price} кредитов`;
+  }
+  
+  console.log(message); 
---
+++ JS/task-6.js
@@ -0,0 +1,9 @@
+let input;
+let total = 0;
+
+do {
+    input = prompt('Введи число');
+    total += Number(input);
+} while(input !== null);
+
+alert(`Общая сумма чисел равна ${total}`);
